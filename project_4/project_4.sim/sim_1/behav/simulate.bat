570e26b91e8ee3cfa149808f5c46dd20c7694935
MuhammadYasir0072 <60392383+MuhammadYasir0072@users.noreply.github.com>
2020-02-09 13:35:39 +0500

    Delete simulate.bat

--- project_4/project_4.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xsim simulation_sixteenbit_camparater_behav -key {Behavioral:sim_1:Functional:simulation_sixteenbit_camparater} -tclbatch simulation_sixteenbit_camparater.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
